8e31cc9703985b03b1c2034f903d76467c03a365
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2023-11-10 18:54:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+    # return 'Hello, World!'
+
+
+@app.route('/results')
+def results():
+    return render_template('result.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
