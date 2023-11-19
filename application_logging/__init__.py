b49d189f66f5f6c580c496415a202deccef31fe8
Ansumanbhujabal <106860608+Ansumanbhujabal@users.noreply.github.com>
2023-11-19 23:18:45 +0530

    Create __init__.py

---
+++ application_logging/__init__.py
@@ -0,0 +1,4 @@
+# application_logging/__init__.py
+
+# This file can be empty or may contain initialization code if needed.
+# It signals to Python that the directory should be treated as a package.
